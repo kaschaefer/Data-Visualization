c0cc1dd7c44aac4163aa9c8897687b25e56be6b0
Kaela Schaefer <kaelaxxanne@gmail.com>
2018-02-27 21:40:33 -0800

    Test program

--- proj8/proj8.cpp
+++ proj8/proj8.cpp
@@ -2,3 +2,23 @@
 #include <vtkContourFilter.h>
 #include <vtkDataSetWriter.h>
 
+int main(){
+    vtkDataSetReader *rdr = vtkDataSetReader::New();
+    rdr->SetFileName("proj8.vtk");
+
+    //Contour the data
+    vtkCountourFilter *cf = vtkCountourFilter::New();
+    cf->SetNumberOfContours(1);
+    cf->SetValue(0, 3.0);
+    cf->SetInputConnection(rdr->GetOutputPort());
+
+    vtkDataSetWriter *wrtr = vtkDataSetWriter::New();
+    wrtr->SetFileName("contour.vtk");
+    wrtr->SetInputConnection(cf->GetOutputPort());
+    wrtr->Write();
+
+    rdr->Delete();
+    cf->Delete();
+    wrter->Delete();
+    
+}
