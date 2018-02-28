63afbe1526c21050abfbc2ecd887bb15798c2620
Kaela Schaefer <kaelaxxanne@gmail.com>
2018-02-27 22:20:22 -0800

    Update to show progress on proj8

--- proj8/proj8.cpp
+++ proj8/proj8.cpp
@@ -3,22 +3,40 @@
 #include <vtkDataSetWriter.h>
 
 int main(){
-    vtkDataSetReader *rdr = vtkDataSetReader::New();
-    rdr->SetFileName("proj8.vtk");
+    //Make Shapes
 
-    //Contour the data
-    vtkCountourFilter *cf = vtkCountourFilter::New();
-    cf->SetNumberOfContours(1);
-    cf->SetValue(0, 3.0);
-    cf->SetInputConnection(rdr->GetOutputPort());
 
-    vtkDataSetWriter *wrtr = vtkDataSetWriter::New();
-    wrtr->SetFileName("contour.vtk");
-    wrtr->SetInputConnection(cf->GetOutputPort());
-    wrtr->Write();
+    vtkSmartPointer<vtkRenderWindow> rendWindow = vtkSmartPointer<vtkRenderWindow>::New();
+    vtkSmartPointer<vtkRenderWindowInteractor> iren = vtkSmartPointer<vtkRenderWindowInteractor>::New();
 
-    rdr->Delete();
-    cf->Delete();
-    wrter->Delete();
+    //Set Up Renderers
+    vtkSmartPointer<vtkRenderer> ren1 = vtkSmartPointer<vtkRenderer>::New();
+    vtkSmartPointer<vtkRenderer> ren2 = vtkSmartPointer<vtkRenderer>::New();
+    vtkSmartPointer<vtkRenderer> ren3 = vtkSmartPointer<vtkRenderer>::New();
+    vtkSmartPointer<vtkRenderer> ren4 = vtkSmartPointer<vtkRenderer>::New();
+
+    //Set Viewports
+    ren1->SetViewport(0, 0, .5, .5);
+    ren2->SetViewport(0, 0.5, 0, 0.5);
+    ren3->SetViewport(0.5, 0.5, 1.0, 1.0);
+    ren4->SetViewport(0.5, 1.0, 0.5, 1.0);
+
+    //Add Renderers to Render Window
+    rendWindow->AddRenderer(ren1);
+    rendWindow->AddRenderer(ren2);
+    rendWindow->AddRenderer(ren3);
+    rendWindow->AddRenderer(ren4);
+
+    //Add Interactor
+    renderWindowInteractor->SetRenderWindow(rendWindow);
     
+
+    //Add the stuff to its respective renderer
+
+    //Invokes Initial Render
+    iren->Initiate();
+    iren->Start();
+
+    return EXIT_SUCCESS;
+
 }
